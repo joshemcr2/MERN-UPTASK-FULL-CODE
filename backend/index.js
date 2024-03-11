e6905181535f873ab5daec1999ff36ced2567dd4
Jose A Muñoz Cardozo <119539448+joshemcr2@users.noreply.github.com>
2024-03-11 10:29:16 -0400

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import conectarDB from "./config/db.js";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
+import proyectoRoutes from "./routes/proyectoRoutes.js";
+import tareaRoutes from "./routes/tareaRoutes.js";
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+
+conectarDB();
+
+//configurar cors
+const whitelist = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de cors"));
+    }
+  },
+};
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+//routing
+app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/proyectos", proyectoRoutes);
+app.use("/api/tareas", tareaRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+const servidor = app.listen(PORT, () => {
+  console.log(`Servidor Corriendo en el Puerto ${PORT}`);
+});
+
